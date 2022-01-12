929d9145128fb861f09fbad7d982324e11223357
fpw2 <fabien.pergaud@gmail.com>
2022-01-12 13:53:59 +0100

    commit 12/01

--- script/modal.js
+++ script/modal.js
@@ -15,12 +15,14 @@ const modalClose = document.querySelector(".close");
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
+const dateControl = document.getElementById("birthdate")
 const quantity = document.getElementById("quantity");
-const radioButton = document.querySelector(".checkbox-input")
+const radios = document.querySelectorAll("input[name=location]");
 const rights = document.getElementById("checkbox1")
-const validate = document.querySelector(".btn-submit");
-const myForm = document.getElementById("inscription");
-
+const modalBody = document.querySelector(".modal-body")
+const form = document.getElementById("inscription")
+const validClose = document.querySelector(".valid-close")
+const submit = document.getElementById("submit")
 
 /**
  * fonction qui permet l'ouverture de la modal
@@ -39,123 +41,138 @@ function closeModal() {
 }
 // close modal event 
 modalClose.addEventListener("click", closeModal);
+validClose.addEventListener("click", closeModal);
+
+// regex email
+var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// regex pour le nom et le prénom
+var identityFormat = /^[a-zA-Z]{2,}$/i;
+// [a-zA-Z] : lettre de A à Z majuscule et minuscule
+// {2,} : 2 et plus
+// $ : regarde ce qui est en accord avec précédemment. ici cela veut dire qu'il matchera uniquement avec des lettres
 
 /**
- * fonction qui vérifie le prénom
+ * Fonction qui vérifie un champ avec une regex
  */
-function validateFirstName() {
-  // [a-zA-Z] : lettre de A à Z majuscule et minuscule
-  // {2,} : 2 et plus
-  const identityFormat = /^[a-zA-Z]{2,}$/i;
-  if (firstName.value.match(identityFormat)) {
-    this.style.border = "2px solid green";
-    this.nextElementSibling.innerText = ""
+function validChamp(champ, regex, messageError) {
+  if (champ.value.match(regex)) {
+    champ.style.border = "2px solid green";
+    champ.nextElementSibling.innerText = ""
+    return true;
   } else {
-    this.nextElementSibling.innerText = "Veuillez entrer 2 caractères ou plus pour le champ Prénom sans chiffre";
-    this.style.border = "2px solid red";
+    champ.nextElementSibling.innerText = messageError;
+    champ.style.border = "2px solid red";
+    return false;
   }
 }
-  firstName.addEventListener("blur", validateFirstName);
-
-  /**
-   * fonction qui vérifie le nom
-   */
-  function validateLastName() {
-    // [a-zA-Z] : lettre de A à Z majuscule et minuscule
-    // {2,} : 2 et plus
-    const identityFormat = /^[a-zA-Z]{2,}$/i;
-    if (lastName.value.match(identityFormat)) {
-      this.style.border = "2px solid green";
-      this.nextElementSibling.innerText = ""
-    } else {
-      this.nextElementSibling.innerText = "Veuillez entrer 2 caractères ou plus pour le champ Nom";
-      this.style.border = "2px solid red";
-    }
-  }
-  lastName.addEventListener("blur", validateLastName);
-
-  /**
-   * fonction qui vérifie l'email
-   */
-  function ValidateEmail() {
-    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (email.value.match(mailFormat)) {
-      this.style.border = "2px solid green";
-      this.nextElementSibling.innerText = ""
-    } else {
-      this.style.border = "2px solid red";
-      this.nextElementSibling.innerText = "Veuillez entrer une adresse e-mail valide";
-    }
-  }
-  email.addEventListener("blur", ValidateEmail);
-
-
-  function validateQuantity() {
-    console.log(quantity.value)
-    if (quantity.value == " ") {
-      console.log(this.style.border)
-      this.style.border = "2px solid red";
-      this.nextElementSibling.innerText = "champ vide";
-    }
-    this.style.border = "2px solid green"
-    this.nextElementSibling.innerText = ""
-  }
-  quantity.addEventListener("blur", validateQuantity);
-
-  /**
-   * fonction qui vérifie qu'un bouton radio est sélectionné
-   */
-  function verifyRadiobutton() {
-    for (var i = 0; i < radioButton.length; i++)
-      if (radioButton[i].checked = true) {} else {
-        alert("cocher une case")
-      }
-  }
-  radioButton.addEventListener("click", verifyRadiobutton)
-
-  /**
-   * fonction qui vérifie que les conditions d'utilisation ont été coché 
-   */
-  function conditionCheked()
-  {
-    if (rights.checked == false) {
-      alert ("accepter les conditions")
-      var newDiv = document.createElement ("span")
-      var newContent = document.createTextNode ("Veuillez cocher les conditions d'utilisation")
-      newDiv.appendChild(newContent)
-      document.body.insertBefore (newDiv, rights)
-    }
-  }
-  rights.addEventListener ("click", conditionCheked)
+firstName.addEventListener("blur", function (e) {
+  validChamp(firstName, identityFormat, "Veuillez entrer 2 caractères ou plus et sans chiffre")
+});
+lastName.addEventListener("blur", function (e) {
+  validChamp(lastName, identityFormat, "Veuillez entrer 2 caractères ou plus et sans chiffre")
+});
+email.addEventListener("blur", function (e) {
+  validChamp(email, mailFormat, "Veuillez entrer une adresse e-mail valide")
+});
 
 /**
- *  fonction qui envoie un message après validation du formulaire
+ * fonction de vérification pour la date
  */
+function validateDate() {
+  if (dateControl.value) { // true
+    dateControl.style.border = "2px solid green";
+    dateControl.nextElementSibling.innerText = ""
+    return true
+  } else {
+    dateControl.style.border = "2px solid red";
+    dateControl.nextElementSibling.innerText = "Veillez entrer votre date de naissance"
+    return false
+  }
+}
+dateControl.addEventListener("blur", validateDate)
 
+/**
+ * fonction qui vérifie qu'un chiffre est rempli
+ */
+function validateQuantity() {
+  if (quantity.value) { // true
+    quantity.style.border = "2px solid green"
+    quantity.nextElementSibling.innerText = ""
+    return true;
+  } else {
+    quantity.style.border = "2px solid red";
+    quantity.nextElementSibling.innerText = "Veuillez entrer un chiffre entre 0 et 99";
+    return false;
+  }
+}
+quantity.addEventListener("blur", validateQuantity);
 
-
-  function disableSubmit(disabled) {
-   if (disabled) {
-     document
-       .getElementById("btn-submit")
-       .setAttribute("disabled", true);
-   } else {
-     document
-       .getElementById("btn-submit")
-       .removeAttribute("disabled");
-   }
- }
- 
-  /**
-   * fonction qui affiche un message de validation du formulaire
-   */
-  myForm.addEventListener("submit", function(e){
-    // j'enlève tous les blancs qu'il pourrait y avoir avec trim()
-    if (firstName.value.trim() == ""){
-      disableSubmit(disabled)
+/**
+ * fonction qui vérifie qu'un bouton radio est sélectionné
+ */
+function verifyRadiobutton() {
+  valueRadiobutton = false;
+  for (var i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      document.querySelector("#radioError").innerText = "";
+      valueRadiobutton = radios[i].value;
+      valueRadiobutton = true
+      console.log(valueRadiobutton)
+      break
+    } else {
+      document.querySelector("#radioError").innerText = "Veuillez choisir une option";
+      valueRadiobutton = false
+      console.log(valueRadiobutton)
     }
+  }
+}
 
+/**
+ * fonction qui vérifie que les conditions d'utilisation ont été coché 
+ */
+function conditionCheked() {
+  if (!rights.checked) {
+    document.querySelector("#checkbox").innerText = "Veuillez accepter les conditions d'utilisation";
+    return false
+  } else {
+    document.querySelector("#checkbox").innerText = "";
+    return true
+  }
+}
+rights.addEventListener('change', conditionCheked)
 
-  })
+// bouton fermer que je mets en display none
+validClose.classList.add("display")
 
+/**
+ *  fonction qui soumet le formulaire
+ *  relié au html dans le form
+ *  par l'attribut onsubmit="validate(event)""
+ */
+function validate(e) {
+  e.preventDefault() // empeche le rechargement de la page
+  validChamp(firstName, identityFormat, "Veuillez entrer 2 caractères ou plus et sans chiffre"),
+    validChamp(lastName, identityFormat, "Veuillez entrer 2 caractères ou plus et sans chiffre"),
+    validChamp(email, mailFormat, "Veuillez entrer une adresse e-mail valide"),
+    validateDate(), validateQuantity(), verifyRadiobutton(), conditionCheked()
+  if (
+    validChamp(firstName, identityFormat, "Veuillez entrer 2 caractères ou plus et sans chiffre") &&
+    validChamp(lastName, identityFormat, "Veuillez entrer 2 caractères ou plus et sans chiffre") &&
+    validChamp(email, mailFormat, "Veuillez entrer une adresse e-mail valide") &&
+    validateDate() &&
+    validateQuantity() &&
+    valueRadiobutton === true &&
+    conditionCheked()
+  ) {
+    //form.submit()
+    validClose.classList.remove("display")
+    modalBody.classList.add("successful")
+    modalBody.innerHTML = "Merci pour votre inscription"
+  }
+}
 
+// Autre façon d'appeler mon formulaire pour le soumettre 
+// const form = document.getElementById("inscription");
+// form.addEventListener("submit", function(e){
+// e.preventDefault()
+// }
